3a2ce6f2e8e180ca942fe3cc02c4813c63aa31c6
EyoNeed <107470882+EyoNeed@users.noreply.github.com>
2024-02-19 17:08:00 +0100

    Add files via upload

---
+++ Resultat.java
@@ -0,0 +1,19 @@
+package cartes;
+
+public class Resultat {
+	private String str;
+	private int entier;
+	
+	public Resultat(String str, int entier) {
+		this.str = str;
+		this.entier = entier;
+	}
+	
+	public String getStr() {
+		return str;
+	}
+	
+	public int getEntier() {
+		return entier;
+	}
+}
